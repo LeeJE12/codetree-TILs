3634e93b4e6eddb938e81ff2044d762ec631a40c
이정은 <144866681+LeeJE12@users.noreply.github.com>
2024-08-27 21:24:25 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240827/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.cpp
+++ 240827/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.cpp
@@ -10,7 +10,6 @@ bool RectOverlap(int x1, int y1, int x2, int y2, int nx1, int ny1, int nx2, int 
     return !(x2 < nx1 || nx2 < x1 || y2 < ny1 || ny2 < y1);
 }
 
-
 int Counter(int x1, int y1, int x2, int y2) {
     int cnt=0;
     for (int i=x1; i<=x2; i++) {
@@ -45,12 +44,6 @@ int FirstRect(int x, int y){
     for(int i = x; i < n; i++) {
         for(int j = y; j < m; j++) {
             cnt = Counter(x, y, i, j) + SecondRect(x, y, i, j);
-
-            if (ans != cnt) {
-                //cout << "x, y, i, j 는 차례대로 " << x << ", " << y << ", " << i << ", " << j << ", " << endl;
-                //cout << "  counter: " << Counter(x, y, i, j) << ", secondRect: " << SecondRect(x, y, i, j) << endl;
-                //cout << "  cnt: " << cnt << ", ans: " << ans << endl;
-            }
             ans = max(ans, cnt);
         }
     }
@@ -71,9 +64,6 @@ int main() {
 
     for (int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
-            //if (maxNum != FirstRect(i, j)) {
-            //cout << i << ", " << j << "에서 다음과 같이 달랐습니다. " << maxNum << ", " << FirstRect(i, j) << endl;
-            
             maxNum = max(maxNum, FirstRect(i, j));
         }
     }
