a1cd8c909b45f3b77787a1a08f24ee81055fff59
이정은 <144866681+LeeJE12@users.noreply.github.com>
2024-08-29 21:27:27 +0900

    Title: 컨베이어 벨트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240829/컨베이어 벨트/conveyor-belt.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int t, n;
+int arr[200];
+
+int main() {
+    cin >> n >> t;
+    for(int i=1; i<=2*n; i++) {
+        cin >> arr[i];
+    }
+    
+    int temp;
+    for(int i=0; i<t; i++) {
+        temp = arr[2*n-i];
+        for(int j=2*n; j >= 1; j--) {
+            arr[j] = arr[j-1];
+        }
+        arr[1] = temp;
+    }
+
+    for(int i=1; i<=n; i++) {
+        cout << arr[i] << " ";
+    } 
+    cout << endl; 
+    for (int i=n+1; i<=2*n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
